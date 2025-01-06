3170620ee98b730a09345470840860906946ebc1
SeoYeonLee12 <134525851+SeoYeonLee12@users.noreply.github.com>
2025-01-06 18:06:54 +0900

    initial commit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+=
+src/Main.java,9/2/924f089edb4af5ee45c8a9d1129cd28e2b368138
+?
+src/No2231.java,c/5/c59f1e9aca75e6cd8ba2da19ce8b03fb63ceae0e
---
+++ src/No2231.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No2231 {
+
+    // 생성자 구하는 함수
+    public static int getConstructor(String N){
+        int num = Integer.parseInt(N); // 부분합
+        int sum=0;
+        // constructor 찾기
+        for(int constructor = 1 ; constructor < num ; constructor++){
+            char [] chars = new char[0];
+            sum=constructor;
+            // 자리수 분할(0번째부터 constructor의 수만큼)
+            for(int i = 0 ; i <= String.valueOf(constructor).length()-1 ; i++){
+                chars = String.valueOf(constructor).toCharArray();
+            }
+            for(char ch : chars){
+                System.out.println(constructor+", "+ch);
+                sum+=Integer.parseInt(String.valueOf(ch));
+            }
+            if(sum==num) {
+                return constructor;
+            }
+
+
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String N = br.readLine();
+        System.out.println(getConstructor(N));
+
+
+    }
+}
