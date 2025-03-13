593c476bff09a136b7bfe83569d3db823e1c8451
SeoYeonLee12 <134525851+SeoYeonLee12@users.noreply.github.com>
2025-03-13 16:28:17 +0900

    feat: solved to 11727 of DP1
    연산의 우선순위가 중요한 것 같다.
    수가 기하급수적으로 커지므로 Long 타입의 배열 생성 후 사용

--- src/dp/dp1/N11727.java
+++ src/dp/dp1/N11727.java
@@ -1,4 +1,28 @@
 package dp.dp1;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class N11727 {
+    static long dp[];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        dp = new long[n + 1];
+
+        dp[0]=dp[1]=1;
+        for (int i = 2; i <= n; i++) {
+            dp[i]= (dp[i-1]+(2*dp[i-2]))%10007;
+        }
+        bw.write(dp[n] + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
