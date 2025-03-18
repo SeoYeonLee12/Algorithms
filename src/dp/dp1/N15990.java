918f26d1ad07d7cb1853ecc92a4bdba743910b17
SeoYeonLee12 <134525851+SeoYeonLee12@users.noreply.github.com>
2025-03-18 16:20:03 +0900

    style: 코드 정렬

--- src/dp/dp1/N15990.java
+++ src/dp/dp1/N15990.java
@@ -8,13 +8,13 @@ import java.io.OutputStreamWriter;
 
 public class N15990 {
     private static long[][] dp;
-    private static int mod= 1000000009;
+    private static int mod = 1000000009;
 
-    private static void add(){
-        for(int i=4; i<=100000; i++){
-            dp[i][1]= (dp[i-1][2]+dp[i-1][3])%mod;
-            dp[i][2]= (dp[i-2][1]+dp[i-2][3])%mod;
-            dp[i][3]= (dp[i-3][2]+dp[i-3][1])%mod;
+    private static void add() {
+        for (int i = 4; i <= 100000; i++) {
+            dp[i][1] = (dp[i - 1][2] + dp[i - 1][3]) % mod;
+            dp[i][2] = (dp[i - 2][1] + dp[i - 2][3]) % mod;
+            dp[i][3] = (dp[i - 3][2] + dp[i - 3][1]) % mod;
         }
     }
 
@@ -23,15 +23,15 @@ public class N15990 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         dp = new long[100001][4];
-        dp[1][1]=dp[2][2]=dp[3][1]=dp[3][2]=dp[3][3]=1;
+        dp[1][1] = dp[2][2] = dp[3][1] = dp[3][2] = dp[3][3] = 1;
         add();
 
-        int T= Integer.parseInt(br.readLine());
-        int number=0;
-        for(int i = 0; i < T; i++){
+        int T = Integer.parseInt(br.readLine());
+        int number = 0;
+        for (int i = 0; i < T; i++) {
             number = Integer.parseInt(br.readLine());
-            long remainder= (dp[number][1]+ dp[number][2]+ dp[number][3])%mod;
-            bw.write(remainder+"\n");
+            long remainder = (dp[number][1] + dp[number][2] + dp[number][3]) % mod;
+            bw.write(remainder + "\n");
         }
         bw.flush();
         bw.close();
